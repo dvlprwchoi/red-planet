048f42d00e7879bb3eb6dfa055b6d753d98d0e3f
dvlprwchoi <dvlpr.wchoi@gmail.com>
2022-01-19 15:10:56 -0600

    Update structure and css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import Main from './components/Main/Main';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="Header">
+        <h1>Red Planet</h1>
+      </div>
+      <Main />
+      <Footer />
     </div>
   );
 }
