480b69bb44af13727c89b256f335d4aaae638c9f
dvlprwchoi <dvlpr.wchoi@gmail.com>
2022-01-19 16:36:55 -0600

    Get array from object api data

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,22 +1,32 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 const API_KEY = process.env.REACT_APP_NASA_API_KEY;
 
 function Main() {
   const API_URL = `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=3000&page=1&api_key=${API_KEY}`;
 
-  async function getImages() {
-    try {
-      const response = await fetch(API_URL);
-      const data = await response.json();
-      console.log(data);
-    } catch (error) {
-      console.error(error);
-    }
-  }
+  const [images, setImages] = useState([]);
+
   useEffect(() => {
+    const getImages = async () => {
+      try {
+        const response = await fetch(API_URL);
+        const data = await response.json();
+        const firstValueFromData = Object.values(data);
+        const firstItemFromArray = firstValueFromData[0];
+        setImages(firstItemFromArray);
+      } catch (error) {
+        console.error(error);
+      }
+    };
     getImages();
   }, []);
-  return <div className="Main">This is main</div>;
+  console.log(images);
+
+  return (
+    <div className="Main">
+      {images.length > 0 ? <h2>Images of Mars</h2> : <h2>Loading images...</h2>}
+    </div>
+  );
 }
 
 export default Main;
